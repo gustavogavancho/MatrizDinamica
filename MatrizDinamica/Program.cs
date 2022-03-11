2cdb02d1c222cf6e27af8b86472d1da60c253384
Gustavo Gavancho León <gustavo.gavancho.l@live.com>
2022-03-10 19:47:23 -0500

    Add project files.

---
+++ MatrizDinamica/Program.cs
@@ -0,0 +1,45 @@
+﻿init:
+int sum = 0, sum1 = 0, valorMaximo = int.MinValue, valorMinimo = int.MaxValue;
+Console.WriteLine("Por favor ingrese el tamaño de la matriz cuadrada: ");
+
+if (int.TryParse(Console.ReadLine(), out int numeroMatriz))
+{
+    Console.WriteLine($"\nHa creado una matriz cuadrada de: {numeroMatriz} x {numeroMatriz}");
+    int[,] matriz = new int[numeroMatriz, numeroMatriz];
+    Random rd = new Random();
+
+    for (int i = 0; i < matriz.GetLength(0); i++)
+    {
+        numeroMatriz = numeroMatriz - 1;
+        for (int j = 0; j < matriz.GetLength(1); j++)
+        {
+            matriz[i, j] = rd.Next(1, 100);
+            if(valorMaximo <= matriz[i, j]) valorMaximo = matriz[i, j];
+            if (valorMinimo >= matriz[i, j]) valorMinimo = matriz[i, j];
+            Console.Write($"{matriz[i, j]}  ");
+            if (i == j) sum1 = sum1 + matriz[i, j];
+            if (j == numeroMatriz) sum = sum + matriz[i, j];
+        }
+        Console.Write("\n");
+    }
+
+    Console.WriteLine($"\nSuma diagonal 1: {sum}");
+    Console.WriteLine($"Suma diagonal 2: {sum1}\n");
+    Console.WriteLine($"Valor máximo: {valorMaximo}");
+    Console.WriteLine($"Valor mínimo: {valorMinimo}\n");
+    Console.Write("Desea volver a empezar => si/no: ");
+    string respuesta  = Console.ReadLine();
+    if (respuesta == "si")
+    {
+        Console.Clear();
+        goto init;
+    }
+        
+}
+else
+{
+    Console.WriteLine("*******************************************************");
+    Console.WriteLine("=> ERROR: Ingrese un valor númerico.");
+    Console.WriteLine("*******************************************************");
+    goto init;
+}
